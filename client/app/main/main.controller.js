cecc306e988d1189ed65a9a3403edb5a5cbe6444
Oab00 <oab93@hotmail.com>
2015-07-06 06:15:13 +0300

    have a chart to work on

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,7 +13,7 @@ angular.module('stocksApp')
       if($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $http.post('/api/things', { name: $scope.newThing.toUpperCase() });
       $scope.newThing = '';
     };
 
@@ -23,5 +23,47 @@ angular.module('stocksApp')
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
+    });
+
+    $(function () {
+        var seriesOptions = [],
+            seriesCounter = 0,
+            names = ['MSFT', 'AAPL', 'GOOG'],
+            // create the chart when all data is loaded
+            createChart = function () {
+
+                $('#container').highcharts('StockChart', {
+
+                    navigator: {
+                      enabled: false
+                    },
+
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2
+                    },
+
+                    series: seriesOptions
+                });
+            };
+
+        $.each(names, function (i, name) {
+
+            $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+                seriesOptions[i] = {
+                    name: name,
+                    data: data
+                };
+
+                // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                // we keep a counter and create the chart when all the data is loaded.
+                seriesCounter += 1;
+
+                if (seriesCounter === names.length) {
+                    createChart();
+                }
+            });
+        });
     });
   });
