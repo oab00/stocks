52a4ec62517d9838899b66c6fd74a4726ea1a2f7
Oab00 <oab93@hotmail.com>
2015-07-06 09:53:58 +0300

    clearing up and final touches

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,12 +6,12 @@ angular.module('stocksApp')
     $scope.warning = null;
     $scope.connectedUsers = 0;
 
-    console.log(socket);
     socket.socket.on('connectedUsers', function(connectedUsers) {
       $scope.connectedUsers = connectedUsers;
     });
 
-    var randomStocks = ['AAPL', 'MSFT', 'GOOG'];
+    var randomStocks = ['AAPL', 'MSFT', 'NHTC', 'XOM', 'MON', 'GLOB',
+                        'STRP', 'AMBA', 'CEA', 'TROV', 'GTT', 'EBIX'];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
@@ -21,6 +21,15 @@ angular.module('stocksApp')
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
+        $scope.warning = null;
+        return;
+      }
+      if($scope.newThing.replace(/[^a-zA-Z]/g, '') === '') {
+        $scope.warning = 'Unwanted characters.';
+        return;
+      }
+      if($scope.newThing.replace(/[^a-zA-Z]/g, '').split('').length > 10) {
+        $scope.warning = 'Stock symbol is too long.';
         return;
       }
 
@@ -29,12 +38,12 @@ angular.module('stocksApp')
 
       for (var i=0; i<$scope.awesomeThings.length; i+=1) {
         if ($scope.awesomeThings[i].name === $scope.newThing.toUpperCase()) {
-          $scope.warning = 'Stock symbol already exists!';
+          $scope.warning = 'Stock symbol already exists.';
           return;
         }
       }
 
-      $http.post('/api/things', { name: $scope.newThing.toUpperCase() });
+      $http.post('/api/things', { name: $scope.newThing.toUpperCase().replace(/[^A-Z]/g, '') });
       $scope.newThing = '';
     };
 
@@ -94,27 +103,8 @@ angular.module('stocksApp')
                         selected: 4
                     },
 
-                    yAxis: {
-                        labels: {
-                            formatter: function () {
-                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                            }
-                        },
-                        plotLines: [{
-                            value: 0,
-                            width: 2,
-                            color: 'silver'
-                        }]
-                    },
-
-                    plotOptions: {
-                        series: {
-                            compare: 'percent'
-                        }
-                    },
-
                     tooltip: {
-                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
                         valueDecimals: 2
                     },
 
@@ -123,21 +113,39 @@ angular.module('stocksApp')
             };
 
         $.each(names, function (i, name) {
+            // 'http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?'
+            $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20' + 
+                      'where%20symbol%20%3D%20%22' + name + '%22%20and%20startDate%20%3D%20%222014-06-07%22%20and%20endDate' + 
+                      '%20%3D%20%222015-06-07%22&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2' + 
+                      'Falltables.env&callback=', function (data) {
 
-            $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+                if (data.query.count === 0) {
+                  console.log(name, 'does not exist.');
+                  
+                } else {
+
+                  data = data.query.results.quote.map(function(dat) {
+                      return [
+                        new Date(dat.Date).getTime(),
+                        parseFloat(parseFloat(dat.High).toFixed(2))
+                      ];
+                    });
+                }
 
                 seriesOptions[i] = {
                     name: name,
                     data: data
                 };
-
+                
                 // As we're loading the data asynchronously, we don't know what order it will arrive. So
                 // we keep a counter and create the chart when all the data is loaded.
                 seriesCounter += 1;
+                
 
                 if (seriesCounter === names.length) {
                     createChart();
                 }
+
             });
         });
     }
